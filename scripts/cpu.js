c1d1466dc014da49b662ba1e196f1e1a201dc0b2
Andrew Ogle <andrewryanogle@gmail.com>
2021-08-15 00:23:40 -0700

    added load rom function

--- scripts/cpu.js
+++ scripts/cpu.js
@@ -24,5 +24,127 @@ class CPU {
     this.paused = false;
 
     this.speed = 10;
+
+    this.stack = new Array();
+  }
+  loadSpritesIntoMemory() {
+    // Array of hex values for each sprite. Each sprite is 5 bytes.
+    // The technical reference provides us with each one of these values.
+    const sprites = [
+      0xf0,
+      0x90,
+      0x90,
+      0x90,
+      0xf0, // 0
+      0x20,
+      0x60,
+      0x20,
+      0x20,
+      0x70, // 1
+      0xf0,
+      0x10,
+      0xf0,
+      0x80,
+      0xf0, // 2
+      0xf0,
+      0x10,
+      0xf0,
+      0x10,
+      0xf0, // 3
+      0x90,
+      0x90,
+      0xf0,
+      0x10,
+      0x10, // 4
+      0xf0,
+      0x80,
+      0xf0,
+      0x10,
+      0xf0, // 5
+      0xf0,
+      0x80,
+      0xf0,
+      0x90,
+      0xf0, // 6
+      0xf0,
+      0x10,
+      0x20,
+      0x40,
+      0x40, // 7
+      0xf0,
+      0x90,
+      0xf0,
+      0x90,
+      0xf0, // 8
+      0xf0,
+      0x90,
+      0xf0,
+      0x10,
+      0xf0, // 9
+      0xf0,
+      0x90,
+      0xf0,
+      0x90,
+      0x90, // A
+      0xe0,
+      0x90,
+      0xe0,
+      0x90,
+      0xe0, // B
+      0xf0,
+      0x80,
+      0x80,
+      0x80,
+      0xf0, // C
+      0xe0,
+      0x90,
+      0x90,
+      0x90,
+      0xe0, // D
+      0xf0,
+      0x80,
+      0xf0,
+      0x80,
+      0xf0, // E
+      0xf0,
+      0x80,
+      0xf0,
+      0x80,
+      0x80, // F
+    ];
+
+    //sprites are stored in the interpreter section of memory starting at hex 0x000
+    for (let i = 0; i < sprites.length; i++) {
+      this.memory[i] = sprites[i];
+    }
+  }
+
+  //programs start at location 0x200
+  loadProgramIntoMemory(program) {
+    for (let loc = 0; loc < program.length; loc++) {
+      this.memory[0x200 + loc] = program[loc];
+    }
+  }
+  loadRom(romName) {
+    const request = new XMLHttpRequest();
+    const self = this;
+
+    // handles the response 
+    request.onerror = function(){
+        // if the request has content 
+        if(request.response){
+            // store the contents in 8 bit array
+            let program = new Uint8Array(request.response);
+
+            // load rom into memory
+            self.loadProgramIntoMemory(program);
+
+        }
+    }
+    // GET roms 
+    request.open('GET', '/roms' + romName);
+    request.responseType = 'arraybuffer';
+
+    request.send();
   }
 }
