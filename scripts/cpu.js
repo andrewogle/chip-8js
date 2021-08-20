4f52b0de449e519b98edae805d94c7082ebd0d86
Andrew Ogle <andrewryanogle@gmail.com>
2021-08-20 10:35:08 -0700

    working on opcode logic

--- scripts/cpu.js
+++ scripts/cpu.js
@@ -175,4 +175,171 @@ class CPU {
       this.speaker.stop();
     }
   }
+  executeInstruction(opcode) {
+    //increment program counter because each instruction is 2 bytes long the PC will increment by 2
+    this.pc += 2;
+
+    //get rid of everything but the 2nd nibble
+    let x = (opcode & 0x0f00) >> 8;
+
+    //get rid of everything but the 3rd nibble
+    let y = (opcode & 0x00f0) >> 4;
+
+    // logic for all 36 instructions
+    switch (opcode & 0xf000) {
+      case 0x0000:
+        switch (opcode) {
+          //00e0 - CLS
+          //clear the display
+          case 0x00e0:
+            this.renderer.clear();
+            break;
+          case 0x00ee:
+            //00ee - RET
+            //pop the last element in the stack array and store it in pc
+            // this will return us from a subroutine
+            this.pc = this.stack.pop();
+            break;
+        }
+
+        break;
+      case 0x1000:
+        //1nnn - jp addr
+        // set the pc to the stored value in nnn
+        this.pc = opcode & 0xfff;
+        break;
+      case 0x2000:
+        //2nnn - call addr
+        this.stack.push(this.pc);
+        this.pc = opcode & 0xfff;
+        break;
+      case 0x3000:
+        // 3xkk - SE Vx, byte
+        //compares the value of the x register to kk
+        //if equal increment by 2
+        if (this.v[x] === (opcode & 0xff)) {
+          this.pc += 2;
+        }
+        break;
+      case 0x4000:
+        // 4xkk - SNE Vx, byte
+        // skips the next instruction are not equal
+        if (this.v[x] !== (opcode & oxff)) {
+          this.pc += 2;
+        }
+        break;
+      case 0x5000:
+        //5xy0 - SE Vx, Vy
+        // if Vx is equal to Vy skip the next instruction
+        if (this.v[x] === this.v[y]) {
+          this.pc += 2;
+        }
+        break;
+      case 0x6000:
+        //6xkk - LD Vx, byte
+        // set the value of Vx to value of kk
+        this.v[x] = opcode & 0xff;
+        break;
+      case 0x7000:
+        //7xkk - ADD Vx, byte
+        // adds kk to Vx
+        this.v[x] += opcode & 0xff;
+        break;
+      case 0x8000:
+        //8xy0 - LD Vx, Vy
+        switch (opcode & 0xf) {
+          // set the value of Vx to the value of Vy
+          case 0x0:
+            this.v[x] = this.v[y];
+            break;
+            //8xy1 - OR Vx, Vy
+            //set Vx to the value of Vx OR Vy
+            this.v[x] |= this.v[y];
+          case 0x1:
+            //8xy2 - AND Vx, Vy
+            // set Vx to value of Vx AND Vy
+            this.v[x] &= this.v[y];
+            break;
+          case 0x2:
+            //8xy3 - XOR Vx, Vy
+            // set Vx to the value of Vx XOR Vy
+            this.v[x] ^= this.v[y];
+            break;
+          case 0x3:
+            //8xy4 - ADD Vx, Vy
+            // sets Vx to Vx + Vy
+            //If the result is greater than 8 bits (i.e., > 255,)
+            //VF is set to 1, otherwise 0.
+            //Only the lowest 8 bits of the result are kept, and stored in Vx
+            let sum = (this.v[x] += this.v[y]);
+
+            this.v[0xf] = 0;
+
+            if (sum > 0xff) {
+              this.v[0xf] = 1;
+            }
+
+            this.v[x] = sum;
+            break;
+          case 0x4:
+            //
+            break;
+          case 0x5:
+            break;
+          case 0x6:
+            break;
+          case 0x7:
+            break;
+          case 0xe:
+            break;
+        }
+
+        break;
+      case 0x9000:
+        break;
+      case 0xa000:
+        break;
+      case 0xb000:
+        break;
+      case 0xc000:
+        break;
+      case 0xd000:
+        break;
+      case 0xe000:
+        switch (opcode & 0xff) {
+          case 0x9e:
+            break;
+          case 0xa1:
+            break;
+        }
+
+        break;
+      case 0xf000:
+        switch (opcode & 0xff) {
+          case 0x07:
+            break;
+          case 0x0a:
+            break;
+          case 0x15:
+            break;
+          case 0x18:
+            break;
+          case 0x1e:
+            break;
+          case 0x29:
+            break;
+          case 0x33:
+            break;
+          case 0x55:
+            break;
+          case 0x65:
+            break;
+        }
+
+        break;
+
+      default:
+        throw new Error("Unknown opcode " + opcode);
+    }
+  }
 }
